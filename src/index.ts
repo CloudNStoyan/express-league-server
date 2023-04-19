11aeb3703ffa91c384cd7b4ab34af1a6aaa9bc9f
Stoyan Kolev <slownessbg@gmail.com>
2023-04-19 05:12:59 +0300

    add riot api

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,51 @@
-import express, { Request, Response } from 'express';
-import * as dotenv from 'dotenv';
-
+import * as dotenv from "dotenv";
 dotenv.config();
+import config from "./config";
+import express, { Request, Response } from "express";
+import axios from "axios";
 
-import config from './config';
+const app = express();
 
-const app = express()
+app.get("/", (req: Request, res: Response) => {
+  res.send("PONG");
+});
 
-app.get('/', (req: Request, res: Response) => {
-  res.send(config.secrets.riotApiToken)
-})
+const riotInstance = axios.create({
+  headers: {
+    "X-Riot-Token": config.secrets.riotApiToken,
+  },
+});
+
+app.get("/matches/:puuid", async (req: Request, res: Response) => {
+  const riotUrl = `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${req.params.puuid}/ids`;
+
+  const riotResponse = await riotInstance.get(riotUrl);
+
+  const matches = await riotResponse.data;
+
+  res.json(matches);
+});
+
+app.get("/match/:matchId", async (req: Request, res: Response) => {
+  const riotUrl = `https://europe.api.riotgames.com/lol/match/v5/matches/${req.params.matchId}`;
+
+  const riotResponse = await riotInstance.get(riotUrl);
+
+  const match = await riotResponse.data;
+
+  res.json(match);
+});
+
+app.get("/summoner/:summonerName", async (req: Request, res: Response) => {
+  const riotUrl = `https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${req.params.summonerName}`;
+
+  const riotResponse = await riotInstance.get(riotUrl);
+
+  const summoner = await riotResponse.data;
+
+  res.json(summoner);
+});
 
 app.listen(config.port, () => {
-  console.log(`Listening on port ${config.port}`)
-})
+  console.log(`Listening on port ${config.port}`);
+});
